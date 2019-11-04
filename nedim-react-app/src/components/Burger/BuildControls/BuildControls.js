7cdc84cea01d0c016b7533ec11a04e38a7c44e44
Nedim Džibrić <47319999+Nedimm23@users.noreply.github.com>
2019-11-04 14:52:05 +0100

    new components added

--- nedim-react-app/src/components/Burger/BuildControls/BuildControls.js
+++ nedim-react-app/src/components/Burger/BuildControls/BuildControls.js
@@ -10,14 +10,21 @@ const controls = [
 ];
 
 const buildControls = (props) => (
-    // eslint-disable-next-line no-unused-expressions
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
           {controls.map(ctrl => (
             <BuildControl 
               key={ctrl.label} 
               label={ctrl.label} 
-              added={() => props.ingredientAdded(ctrl.type)}/>  
-          ))}      
+              added={() => props.ingredientAdded(ctrl.type)}
+              removed={() => props.ingredientRemoved(ctrl.type)} 
+              disabled ={props.disabled[ctrl.type]} 
+            />  
+          ))} 
+        <button 
+            className={classes.OrderButton} 
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>     
     </div>
 );
 
